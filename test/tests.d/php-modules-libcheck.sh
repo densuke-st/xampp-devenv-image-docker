44b5af90f78538c74d39a79c09bd688d2806d26b
SATO Daisuke <densuke+github@gmail.com>
2025-06-18 17:26:54 +0900

    *.soがない時のロジックを追加(無いとおかしいのでエラー終了)

--- test/tests.d/php-modules-libcheck.sh
+++ test/tests.d/php-modules-libcheck.sh
@@ -8,10 +8,17 @@ fi
 echo "PHP extension directory: $LIBPATH"
 cd ${LIBPATH} || exit 1
 
+# *.soのファイルがひとつも無いのはむしろおかしいのでエラーとしてここでたたき落とす
+set -- *.so
+if [ "$1" = "*.so" ] || [ $# -eq 0 ]; then
+  echo "Error: No PHP extension libraries found."
+  exit 1
+fi
+
 # 拡張モジュールのDLLをチェックする
 for dll in *.so; do
     echo -n "Checking dependencies for ${dll}..."
-    ldd "${dll}" | grep  "not found" && exit 1
+    ldd "${dll}"  2>&1 | grep -q "not found" && exit 1
     echo "OK"
 done
 echo "All PHP extension libraries have their dependencies satisfied."
--- test/tests.d/python-module-dll.sh
+++ test/tests.d/python-module-dll.sh
@@ -13,6 +13,12 @@ fi
 
 cd "${LIBDIR}" || exit 1
 
+# *.soが全くないのはおかしいので、この時点でたたき落としておく
+set -- *.so
+if [ "$1" = "*.so" ] || [ $# -eq 0 ]; then
+  echo "Error: No Python extension libraries found."
+  exit 1
+
 for dll in *.so; do
     echo -n "Checking dependencies for ${dll}..."
     if ! ldd "${dll}" 2>&1 | grep -q "not found"; then
